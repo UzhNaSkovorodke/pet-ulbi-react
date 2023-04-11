c4c1488cbbf1943759bea48f0d635f1543a525d6
UzhNaSkovorodke <piratt.kiri@gmail.com>
2023-04-11 21:59:51 +0700

    react-router add

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,23 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
 
-import PostList from "./components/PostList";
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/MyModal";
-import MyButton from "./components/UI/button/MyButton";
-import { usePosts } from "./hooks/usePosts";
-import PostService from "./API/PostService";
-import useFetching from "./hooks/useFetching";
-import { getPageCount, getPagesArray } from "./utils/page";
-import Pagination from "./components/UI/pagination/Pagination";
+const Case = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Posts />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/posts" element={<Posts />} />
+    </Routes>
+  );
+};
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({ sort: "", query: "" });
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
-    const totalCount = response.headers["x-total-count"];
-    setTotalPages(getPageCount(totalCount, limit));
-  });
-  // в массиве зависимостей pagr т.к состояние в react ассинхронное и это такое решение того что использутеся состояние с отставанием
-  useEffect(() => {
-    fetchPosts();
-  }, [page]);
-
-  //ниже функция создания поста
-  const createPost = function (newPost) {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  //ниже функция удаления поста
-  const removePost = function (post) {
-    //так реализована фильтрация, просто сравниваем в массиве если у поста id равен с id постом который мы удаляем, то происходит фильтрация этого поста
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-  const changePage = function (page) {
-    setPage(page);
-  };
-
   return (
-    <div className="App">
-      <MyButton onClick={() => setModal(true)}>Создать пользователя</MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm createPost={createPost} />
-      </MyModal>
-      <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && <h1>Произошла ошибка ${postError}</h1>}
-      {isPostLoading ? (
-        <h1>Идет загрузка....</h1>
-      ) : (
-        <PostList
-          removePost={removePost}
-          //в posts передает отсортированный и отфильтрованный массив
-          posts={sortedAndSearchedPosts}
-          title={"Список постов"}
-        />
-      )}
-      <Pagination page={page} changePage={changePage} totalPages={totalPages} />
-    </div>
+    <BrowserRouter>
+      <Case />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+
+import PostList from "../components/PostList";
+import PostForm from "../components/PostForm";
+import PostFilter from "../components/PostFilter";
+import MyModal from "../components/MyModal";
+import MyButton from "../components/UI/button/MyButton";
+import { usePosts } from "../hooks/usePosts";
+import PostService from "../API/PostService";
+import useFetching from "../hooks/useFetching";
+import { getPageCount, getPagesArray } from "../utils/page";
+import Pagination from "../components/UI/pagination/Pagination";
+import { Link } from "react-router-dom";
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
+  });
+  // в массиве зависимостей pagr т.к состояние в react ассинхронное и это такое решение того что использутеся состояние с отставанием
+  useEffect(() => {
+    fetchPosts();
+  }, [page]);
+
+  //ниже функция создания поста
+  const createPost = function (newPost) {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+
+  //ниже функция удаления поста
+  const removePost = function (post) {
+    //так реализована фильтрация, просто сравниваем в массиве если у поста id равен с id постом который мы удаляем, то происходит фильтрация этого поста
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+  const changePage = function (page) {
+    setPage(page);
+  };
+
+  return (
+    <div className="App">
+      <nav style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <Link to="/posts">Posts</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <MyButton onClick={() => setModal(true)}>Создать пользователя</MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm createPost={createPost} />
+      </MyModal>
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>Произошла ошибка ${postError}</h1>}
+      {isPostLoading ? (
+        <h1>Идет загрузка....</h1>
+      ) : (
+        <PostList
+          removePost={removePost}
+          //в posts передает отсортированный и отфильтрованный массив
+          posts={sortedAndSearchedPosts}
+          title={"Список постов"}
+        />
+      )}
+      <Pagination page={page} changePage={changePage} totalPages={totalPages} />
+    </div>
+  );
+}
+
+export default Posts;
